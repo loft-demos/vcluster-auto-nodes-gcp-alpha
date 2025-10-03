6fa63dda9f7832d61a8aab0823cc97d652d5e3e8
Kurt Madel <kurt.madel@loft.sh>
2025-10-03 12:14:27 -0400

    Update outputs.tf

--- environment/infrastructure/outputs.tf
+++ environment/infrastructure/outputs.tf
@@ -7,8 +7,3 @@ output "subnet_name" {
   value = module.vpc.subnets["${local.region}/${local.private_subnet_name}"].name
   sensitive = true
 }
-
-output "service_account_email" {
-  value = google_service_account.ccm_csi.email
-  sensitive = true
-}
